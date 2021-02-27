19e853a18098c5e83f27116a8bfacb59b299394f
bhmueller <s6bemuel@uni-bonn.de>
2021-02-27 23:23:15 +0100

    Modularise function: define function for h_matrix outside shapley_moebius.

--- shapley_moebius.py
+++ shapley_moebius.py
@@ -6,14 +6,6 @@ import numpy as np
 import chaospy as cp
 
 
-def model(x):
-    return np.sum(x)
-
-
-def trafo(x):
-    return x
-
-
 def shapley_moebius(k, n, model, trafo):
     """
     Estimate Shapley effects via Möbius inverse.
@@ -43,39 +35,16 @@ def shapley_moebius(k, n, model, trafo):
     u = cp.create_sobol_samples(
         n, 2 * k, 123
     ).T  # dim = 2 * k, since want two data sets.
-    # assert_array_equal(u.shape, np.array([n, 2 * k]))
-    # u = np.random.uniform(0, 1, n)
-    # u[1, :] = []  # Delete row 1 in matrix u.
     # u has n rows and 2 * k columns.
-    x_a = trafo(u[:, 0:k])  # First k elements.
-    x_b = trafo(u[:, k:])  # Last k elements.
-    y_a = model(x_a)
-    y_b = model(x_b)
+    x_a = trafo(u[:, 0:k])
+    x_b = trafo(u[:, k:])
 
     n_subsets = np.power(2, k) - 1  # l: number of non-vanishing input subsets.
-    inputs_encoded = np.power(2, np.arange(k))
-    h_matrix = np.zeros((2, n_subsets))  # H
-    subset_size = np.zeros((1, n_subsets))  # sz
+    subset_encoder = np.power(2, np.arange(k))
 
-    # Iterate over all subsets.
-    for i in np.arange(n_subsets):
-        # g is set of inputs considered, binary encoded.
-        g = (
-            np.bitwise_and(
-                i + 1,
-                inputs_encoded,
-            )
-            != 0
-        )
-        subset_size[:, i] = np.sum(
-            g
-        )  # Sum over boolean: True/False interpreted as 1/0.
-        x_i = x_a.copy()  # x_i is always set to x_a again.
-        # g_0 = np.where(g == False)
-        x_i[:, g] = x_b[:, g].copy()  # Some are replaced by values from x_b.
-        y_i = model(x_i)
-        # Fill columns of h_matrix one by one.
-        h_matrix[:, i] = [np.mean(np.power((y_i - y_a), 2)), (y_b.T * (y_i - y_a)) / n]
+    h_matrix, subset_size = _calc_h_matrix(
+        n, model, x_a, x_b, n_subsets, subset_encoder
+    )
 
     mob = np.zeros((2, n_subsets))
     sel = 1
@@ -104,3 +73,32 @@ def shapley_moebius(k, n, model, trafo):
     variance = h_matrix[:, -1]
 
     return shapley_effects, variance
+
+
+def _calc_h_matrix(n, model, x_a, x_b, n_subsets, subset_encoder):
+
+    y_a = model(x_a)
+    y_b = model(x_b)
+
+    h_matrix = np.zeros((2, n_subsets))  # H
+    subset_size = np.zeros((1, n_subsets))  # sz
+
+    for i in np.arange(n_subsets):
+        # g is set of inputs considered, binary encoded.
+        g = (
+            np.bitwise_and(
+                i + 1,
+                subset_encoder,
+            )
+            != 0
+        )
+        subset_size[:, i] = np.sum(
+            g
+        )  # Sum over boolean: True/False interpreted as 1/0.
+        x_i = x_a.copy()  # x_i is always set to x_a again.
+        x_i[:, g] = x_b[:, g].copy()  # Some are replaced by values from x_b.
+        y_i = model(x_i)
+        # Fill columns of h_matrix one by one.
+        h_matrix[:, i] = [np.mean(np.power((y_i - y_a), 2)), (y_b.T * (y_i - y_a)) / n]
+
+    return h_matrix, subset_size
