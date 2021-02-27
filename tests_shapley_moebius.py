c4d2a59a3f21e8025e39d6e95a76eda3065b8124
bhmueller <s6bemuel@uni-bonn.de>
2021-02-27 20:59:28 +0100

    [WIP] Add test for h_matrix having no elements that equal float 0.

--- tests_shapley_moebius.py
+++ tests_shapley_moebius.py
@@ -1,16 +1,40 @@
 """
 This file contains tests for shapley_moebius.py.
 """
+import numpy as np
+from numpy.testing import assert_array_equal
 from shapley_moebius import shapley_moebius
 from auxiliary_functions import ishigami_function
 
 
 def test_bitwise_and():
-    print(1)
+    k = 4
+    inputs_encoded = np.power(2, np.arange(k))
+    i = 3
+
+    g = (
+        np.bitwise_and(
+            i + 1,
+            inputs_encoded,
+        )
+        != 0
+    )
+
+    g_expected = np.array([True, True, False, False])
+
+    assert_array_equal(g, g_expected)
 
 
-def test_h_matrix_one_column():
+def test_h_matrix_non_zero():
+    """
+    This test should pass if all elements in h_matrix are unequal zero.
+    """
     print(1)
+    #
+
+    # zero_array
+
+    # assert_raises(AssertionError, assert_array_equal, h_matrix_expected, zero_array)
 
 
 def test_mob():
