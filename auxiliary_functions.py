3788fb16fd2a417b734e5dd7a0b5280b8deb4fe0
bhmueller <s6bemuel@uni-bonn.de>
2021-02-27 12:53:05 +0100

    Add functions for test cases I want to implement. Rename Ishigami function.

--- auxiliary_functions.py
+++ auxiliary_functions.py
@@ -5,5 +5,5 @@ This module contains functions needed for the tests of shapley_moebius.py.
 import numpy as np
 
 
-def _ishigami_function(x):
+def ishigami_function(x):
     return np.sin(x[:, 1]) * (1 + 0.1 * (x[:, 3] ** 4)) + 7 * (np.sin(x[:, 2]) ** 2)
---
+++ tests_shapley_moebius.py
@@ -0,0 +1,33 @@
+"""
+This file contains tests for shapley_moebius.py.
+"""
+from shapley_moebius import shapley_moebius
+from auxiliary_functions import ishigami_function
+
+
+def test_bitwise_end():
+    print(1)
+
+
+def test_h_matrix_one_column():
+    print(1)
+
+
+def test_mob():
+    print(1)
+
+
+def test_sum_mob():
+    print(1)
+
+
+def test_ishigami():
+    """
+    This test setting is taken from Iooss, Betrand and Clémentine Prieur. 2019. Shapley
+    effects for sensitivity analysis with correlated inputs: comparisons with Sobol’
+    indices, numerical estimation and applications. hal-01556303v6"""
+
+    # k = 3
+    # n = 10
+    # model = ishigami_function
+    # trafo = rosenblatt_transformation
