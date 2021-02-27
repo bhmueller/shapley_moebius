362e93c027c193b65ce27cbb708c395ba0187e87
bhmueller <s6bemuel@uni-bonn.de>
2021-02-27 21:01:25 +0100

    Fix bug: due to encoding of index 0, h_matrix had zeros in first column. Add i + 1 to g.

--- shapley_moebius.py
+++ shapley_moebius.py
@@ -6,6 +6,14 @@ import numpy as np
 import chaospy as cp
 
 
+def model(x):
+    return np.sum(x)
+
+
+def trafo(x):
+    return x
+
+
 def shapley_moebius(k, n, model, trafo):
     """
     Estimate Shapley effects via Möbius inverse.
@@ -32,11 +40,14 @@ def shapley_moebius(k, n, model, trafo):
 
     # Better use chaospy.create_sobol_samples(order, dim, seed=1)
     # order: no. of unique samples, dim: no. of spacial dim.s, i.e. no. of inputs.
-    u = cp.create_sobol_samples(n, 2 * k, 123)  # dim = 2 * k, since want two data sets.
+    u = cp.create_sobol_samples(
+        n, 2 * k, 123
+    ).T  # dim = 2 * k, since want two data sets.
+    # assert_array_equal(u.shape, np.array([n, 2 * k]))
     # u = np.random.uniform(0, 1, n)
     # u[1, :] = []  # Delete row 1 in matrix u.
     # u has n rows and 2 * k columns.
-    x_a = trafo(u[:, 0 : k - 1])  # First k elements.
+    x_a = trafo(u[:, 0:k])  # First k elements.
     x_b = trafo(u[:, k:])  # Last k elements.
     y_a = model(x_a)
     y_b = model(x_b)
@@ -48,26 +59,29 @@ def shapley_moebius(k, n, model, trafo):
 
     # Iterate over all subsets.
     for i in np.arange(n_subsets):
-        # g is set of inputs considered.
+        # g is set of inputs considered, binary encoded.
         g = (
             np.bitwise_and(
-                i,
+                i + 1,
                 inputs_encoded,
             )
             != 0
         )
-        subset_size[i] = np.sum(g)
-        x_i = x_a  # x_i is always set to x_a again?
-        x_i[:, g] = x_b[:, g]
+        subset_size[:, i] = np.sum(
+            g
+        )  # Sum over boolean: True/False interpreted as 1/0.
+        x_i = x_a.copy()  # x_i is always set to x_a again.
+        # g_0 = np.where(g == False)
+        x_i[:, g] = x_b[:, g].copy()  # Some are replaced by values from x_b.
         y_i = model(x_i)
         # Fill columns of h_matrix one by one.
-        h_matrix[:, i] = [np.mean(np.power((y_i - y_a), 2)), y_b.T * (y_i - y_a) / n]
+        h_matrix[:, i] = [np.mean(np.power((y_i - y_a), 2)), (y_b.T * (y_i - y_a)) / n]
 
     mob = np.zeros((2, n_subsets))
     sel = 1
 
     for i in np.arange(n_subsets):
-        ii = np.nonzero(sel)
+        ii = np.nonzero(sel)  # First sel = 1 (see above).
         mob[:, i] = (
             h_matrix[:, ii] * np.power(-1, subset_size[i] + subset_size[ii].T)
         ) / subset_size[i]
@@ -76,10 +90,10 @@ def shapley_moebius(k, n, model, trafo):
     shapley_effects = np.ones((2, k))
 
     for i in np.arange(k):
-        # sum() in Matlab sums elements in each row and returns a column vector.
+        # sum(dim=2) in Matlab sums elements in each row and returns a column vector.
         shapley_effects[:, i] = np.sum(
             mob[
-                :,
+                :,  # Below line is faulty.
                 np.where[np.bitwise_and(np.arange(n_subsets), np.power(2, i - 1)) != 0],
             ],
             1,
