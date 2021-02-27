575e3fac3d38a8d42ca4917bfae9b7fe9ac132c5
bhmueller <s6bemuel@uni-bonn.de>
2021-02-27 13:28:44 +0100

    Add sampling from Sobol seuqence.

--- shapley_moebius.py
+++ shapley_moebius.py
@@ -3,6 +3,7 @@ This module implements algorithm 5.1 from Plischke, Elmar, Giovanni Rabitti, Ema
 Borgonovo. 2020. Computing Shapley Effects for Sensitivity Analysis. arXiv.
 """
 import numpy as np
+import chaospy as cp
 
 
 def shapley_moebius(k, n, model, trafo):
@@ -30,24 +31,26 @@ def shapley_moebius(k, n, model, trafo):
     """
 
     # Better use chaospy.create_sobol_samples(order, dim, seed=1)
-    u = np.uniform([0, 1])
-    x_a = trafo(u)
-    x_b = trafo(u)
+    # order: no. of unique samples, dim: no. of spacial dim.s, i.e. no. of inputs.
+    u = cp.create_sobol_samples(n, 2 * k, 123)  # dim = 2 * k, since want two data sets.
+    # u = np.random.uniform(0, 1, n)
+    # u[1, :] = []  # Delete row 1 in matrix u.
+    # u has n rows and 2 * k columns.
+    x_a = trafo(u[:, 0 : k - 1])  # First k elements.
+    x_b = trafo(u[:, k:])  # Last k elements.
     y_a = model(x_a)
     y_b = model(x_b)
 
-    n_subsets = np.power(2, k) - 1  # l
+    n_subsets = np.power(2, k) - 1  # l: number of non-vanishing input subsets.
     h_matrix = np.zeros((2, n_subsets))  # H
     subset_size = np.zeros((1, n_subsets))  # sz
 
+    # Iterate over all subsets.
     for i in np.arange(n_subsets):
         # g is set of inputs considered.
         g = np.bitwise_and(
             i,
-            np.power(
-                2,
-            )
-            != 0,
+            np.power(2, np.arange(0, k - 1)) != 0,
         )  # Here sth. missing.
         subset_size[i] = np.sum(g)
         x_i = x_a  # x_i is always set to x_a again?
@@ -77,7 +80,7 @@ def shapley_moebius(k, n, model, trafo):
             ],
             1,
         )
-    # In Numpy column is axis=1. In Matlab, dim = 2 is a column; dimension in Matlab:
+    # In Numpy column is axis = 1. In Matlab, dim = 2 is a column; dimension in Matlab:
     # 1 = row, 2 = column.
 
     variance = h_matrix[:, -1]
