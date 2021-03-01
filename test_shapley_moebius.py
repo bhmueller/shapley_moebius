e704fb3f5b33661bbeb4592e0992fcc71fe3d22e
bhmueller <s6bemuel@uni-bonn.de>
2021-03-01 11:12:40 +0100

    Finish test for h_matrix being nonzero via 'with pytest.raises'.

--- test_shapley_moebius.py
+++ test_shapley_moebius.py
@@ -4,6 +4,7 @@ This file contains tests for shapley_moebius.py.
 import numpy as np
 import chaospy as cp
 import operator
+import pytest
 from numpy.testing import assert_array_compare
 from shapley_moebius import shapley_moebius
 from shapley_moebius import _calc_h_matrix
@@ -59,7 +60,8 @@ def test_h_matrix_non_zero():
 
     zero_array = np.zeros((2, n_subsets))
 
-    assert_array_compare(operator.__ne__, h_matrix_expected, zero_array)
+    with pytest.raises(AssertionError):
+        assert_array_compare(operator.__ne__, h_matrix_expected, zero_array)
 
 
 def test_mob():
