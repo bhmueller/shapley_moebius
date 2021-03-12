f6dacda10d78f92ac3a8cd33416f4dbada62723f
bhmueller <s6bemuel@uni-bonn.de>
2021-03-12 17:05:30 +0100

    Add argument 'seed' to get_expected_values.

--- auxiliary_functions.py
+++ auxiliary_functions.py
@@ -7,32 +7,33 @@ import chaospy as cp
 import scipy.io
 from scipy.stats import norm
 
-# from oct2py import octave as oct
+from oct2py import octave as oct
 
 
 def ishigami_function(x):
     return np.sin(x[:, 0]) * (1 + 0.1 * (x[:, 2] ** 4)) + 7 * (np.sin(x[:, 1]) ** 2)
 
 
-# def get_test_values_additive_uniform(k, n):
-#     np.random.seed(123)
-#     u = cp.create_sobol_samples(n, 2 * k, 123).T
-#     scipy.io.savemat('n_inputs.mat', {'k': k})
-#     scipy.io.savemat('n_samples.mat', {'n': n})
-#     scipy.io.savemat('uniform_data.mat', {'u': u})
-#     # Change directory.
-#     oct.eval("cd C:/Users/admin/shapley_moebius")
-#     # Run .m file.
-#     oct.eval("shapmob_testing")
-#     # oct.eval("save -v7 myworkspace.mat")
+def get_test_values_additive_uniform(k, n, seed):
 
-#     expected_results = {
-#         'subset_size': scipy.io.loadmat('subset_size_results.mat')['sz'],
-#         'h_matrix': scipy.io.loadmat('h_matrix_results.mat')['H'],
-#         'mob': scipy.io.loadmat('mob_results.mat')['mob'],
-#         'variance': scipy.io.loadmat('variance_results.mat')['V'],
-#         'shapley_effects': scipy.io.loadmat('shapley_effects_results.mat')['Shap'],}
-#     return expected_results
+    u = cp.create_sobol_samples(n, 2 * k, seed).T
+    scipy.io.savemat("data/n_inputs.mat", {"k": k})
+    scipy.io.savemat("data/n_samples.mat", {"n": n})
+    scipy.io.savemat("data/uniform_data.mat", {"u": u})
+    # Change directory.
+    oct.eval("cd C:/Users/admin/shapley_moebius/data")
+    # Run .m file.
+    oct.eval("get_expected_values")
+    oct.eval("save -v7 myworkspace.mat")
+
+    expected_results = {
+        "subset_size": scipy.io.loadmat("data/subset_size_results.mat")["sz"],
+        "h_matrix": scipy.io.loadmat("data/h_matrix_results.mat")["H"],
+        "mob": scipy.io.loadmat("data/mob_results.mat")["mob"],
+        "variance": scipy.io.loadmat("data/variance_results.mat")["V"],
+        "shapley_effects": scipy.io.loadmat("data/shapley_effects_results.mat")["Shap"],
+    }
+    return expected_results
 
 
 def transformation_mvnorm(u):
