c0475daeba4cc60bf0601215dff9ecb7b8aab2ea
bhmueller <s6bemuel@uni-bonn.de>
2021-03-21 15:46:59 +0100

    Add argument correlation for get_expected_values to get values for either dependent or independent inputs.

--- auxiliary_functions.py
+++ auxiliary_functions.py
@@ -14,7 +14,7 @@ def ishigami_function(x):
     return np.sin(x[:, 0]) * (1 + 0.1 * (x[:, 2] ** 4)) + 7 * (np.sin(x[:, 1]) ** 2)
 
 
-def get_test_values_additive_uniform(k, n, seed):
+def get_test_values_additive_uniform(k, n, seed, correlation):
 
     u = cp.create_sobol_samples(n, 2 * k, seed).T
     scipy.io.savemat("data/n_inputs.mat", {"k": k})
@@ -23,8 +23,13 @@ def get_test_values_additive_uniform(k, n, seed):
     # Change directory.
     oct.eval("cd C:/Users/admin/shapley_moebius/data")
     # Run .m file.
-    oct.eval("get_expected_values")
-    oct.eval("save -v7 myworkspace.mat")
+    if correlation == "independent":
+        oct.eval("get_expected_values_independent")
+    elif correlation == "dependent":
+        oct.eval("get_expected_values_dependent")
+    else:
+        print("Please specify correlation.")
+    # oct.eval("save -v7 myworkspace.mat")
 
     expected_results = {
         "subset_size": scipy.io.loadmat("data/subset_size_results.mat")["sz"],
