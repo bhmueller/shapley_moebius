76210f0f66a360d70c5850082eccbe19a2acf724
bhmueller <s6bemuel@uni-bonn.de>
2021-03-24 23:56:08 +0100

    Add test for s_matrix and samples n_a, c_b, x_a and x_b.

--- test_shapley_moebius_dependent.py
+++ test_shapley_moebius_dependent.py
@@ -9,7 +9,9 @@ from numpy.testing import assert_array_almost_equal
 from numpy.testing import assert_array_equal
 from shapley_moebius_dependent import shapley_moebius_dependent
 from shapley_moebius_dependent import _calc_h_matrix_dependent
+from shapley_moebius_dependent import _sample_data
 from auxiliary_functions import get_test_values_additive_uniform
+from auxiliary_functions import get_test_values_sample_data
 from auxiliary_functions import linear_model
 from auxiliary_functions import trafo_normal
 
@@ -31,6 +33,53 @@ def test_shapley_moebius_dependent():
     random_mode = "Sobol"
 
     shapley_moebius_dependent(k, n, model, trafo, rank_corr, random_mode)
+
+
+def test_s_matrix():
+    rank_corr = np.array([[1, -0.5, 0.5], [-0.5, 1, 0], [0.5, 0, 1]])
+    s_matrix = np.linalg.cholesky(rank_corr)
+
+    expected = np.array(
+        [
+            [1.00000, -0.50000, 0.50000],
+            [0.00000, 0.86603, 0.28868],
+            [0.00000, 0.00000, 0.81650],
+        ]
+    )
+
+    assert_array_almost_equal(s_matrix, expected, decimal=5)
+
+
+def test_sample_data():
+    """Test for data transformed to a certain non-standard normal distribution."""
+
+    k = 3
+    n = 10
+    seed = 123
+    u = cp.create_sobol_samples(n, 2 * k, seed).T
+
+    mu = np.zeros(k)
+    var_1 = 16
+    var_2 = 4
+    var_3 = 9
+    var = np.array([var_1, var_2, var_3])
+
+    rank_corr = np.array([[1, -0.5, 0.5], [-0.5, 1, 0], [0.5, 0, 1]])
+    s_matrix = np.linalg.cholesky(rank_corr)
+
+    trafo = partial(trafo_normal, mu=mu, var=var)
+
+    expected_results = get_test_values_sample_data(k, n, seed, rank_corr)
+
+    n_a, c_b, x_a, x_b = _sample_data(s_matrix, u, k, trafo)
+
+    assert_array_almost_equal(n_a, expected_results["n_a"])
+
+    assert_array_almost_equal(c_b, expected_results["c_b"])
+
+    assert_array_almost_equal(x_a, expected_results["x_a"])
+
+    assert_array_almost_equal(x_b, expected_results["x_b"])
 
 
 def test_calc_h_matrix_dependent():
@@ -103,7 +152,7 @@ def test_linear_two_inputs():
     trafo = partial(trafo_normal, mu=mu, var=var)
 
     k = 2
-    n = 10 ** 7
+    n = 10 ** 6
     rank_corr = np.array([[1, rho], [rho, 1]])
     random_mode = "Sobol"
 
@@ -151,7 +200,7 @@ def test_linear_three_inputs():
     trafo = partial(trafo_normal, mu=mu, var=var)
 
     k = 3
-    n = 10 ** 7
+    n = 10 ** 6
     rank_corr = np.array([[1, 0, 0], [0, 1, rho], [0, rho, 1]])
     random_mode = "Sobol"
 
