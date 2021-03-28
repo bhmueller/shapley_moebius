7bb6bb8db3d902a3298ef3363c4bb3cd6285a320
bhmueller <s6bemuel@uni-bonn.de>
2021-03-28 23:18:04 +0200

    Delete unnecessary comments.

--- shapley_moebius_dependent.py
+++ shapley_moebius_dependent.py
@@ -50,14 +50,12 @@ def shapley_moebius_dependent(k, n, model, trafo, rank_corr, random_mode):
         print("Error. Please specify random_mode by either random or Sobol")
 
     n_subsets = np.power(2, k) - 1
-    # power_sequence = np.power(2, k) - 1
 
     h_matrix, subset_size, evals = _calc_h_matrix_dependent(
         k, n, u, model, trafo, n_subsets, rank_corr
     )
 
     # Get Shapley effects: As in fct. with indep. inputs.
-
     mob = _calc_mob(n_subsets, h_matrix, subset_size)
 
     shapley_effects, variance = _calc_shapley_effects(k, n_subsets, mob, h_matrix)
@@ -67,7 +65,6 @@ def shapley_moebius_dependent(k, n, model, trafo, rank_corr, random_mode):
 
 def _sample_data(s_matrix, u, k, trafo):
     n_a = norm.ppf(u[:, 0:k])  # Normal a sample.
-    # WIP: Correct?
     x_a = trafo(norm.cdf(np.dot(n_a, s_matrix)))
     c_b = np.dot(norm.ppf(u[:, k:]), s_matrix)  # Correlated b sample.
     x_b = trafo(norm.cdf(c_b))
@@ -76,8 +73,6 @@ def _sample_data(s_matrix, u, k, trafo):
 
 
 def _calc_h_matrix_dependent(k, n, u, model, trafo, n_subsets, rank_corr):
-
-    # WIP: Transposed or not? It worked with the non-transposed Chol matrix.
     s_matrix = np.linalg.cholesky(rank_corr).T
 
     n_a, c_b, x_a, x_b = _sample_data(s_matrix, u, k, trafo)
@@ -104,20 +99,23 @@ def _calc_h_matrix_dependent(k, n, u, model, trafo, n_subsets, rank_corr):
         g_current = np.nonzero(g[i])[0]
         g_complement = np.where(g[i] == 0)[0]
 
-        element_1 = rank_corr[g_current, :][:, g_current]
-        element_2 = rank_corr[g_current, :][:, g_complement]
-        element_3 = rank_corr[g_complement, :][:, g_current]
-        element_4 = rank_corr[g_complement, :][:, g_complement]
-
         # Construct new array.
-        new_1 = np.hstack((element_1, element_2))
-        new_2 = np.hstack((element_3, element_4))
+        new_1 = np.hstack(
+            (
+                rank_corr[g_current, :][:, g_current],
+                rank_corr[g_current, :][:, g_complement],
+            )
+        )
+        new_2 = np.hstack(
+            (
+                rank_corr[g_complement, :][:, g_current],
+                rank_corr[g_complement, :][:, g_complement],
+            )
+        )
         helper_matrix = np.vstack((new_1, new_2))
 
         d_matrix = np.linalg.cholesky(helper_matrix).T
 
-        # WIP: Is index correct? Do we need current_subset_size + 1 in the second and
-        # third case?
         d_11 = d_matrix[current_subset_size:, current_subset_size:]
         d_22 = d_matrix[0:current_subset_size, 0:current_subset_size]
         d_21 = d_matrix[0:current_subset_size, current_subset_size:]
